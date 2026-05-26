df35fbfdd60205087cd383f177e76ad63669ba2b
Lucas <lucaseuof07@gmail.com>
2026-05-26 15:42:49 -0300

    alterando base url da api para a url do deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: "https://tcc-backend-bp8z.onrender.com/"
 });
 
 // Interceptor para incluir o token em todas as requisições
